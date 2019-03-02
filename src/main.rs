e20af396d24f6256db61c8b83d1c69bffba837b6
Dmitry Serov <barbari100@gmail.com>
2019-03-02 13:16:39 +0700

    Add multispectral module stub

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+extern crate image;
+
+mod multispectral;
+
 fn main() {
-    let a = 1 + 2;
-    println!("Hello, world!");
+    let img_path = std::env::args().nth(1)
+        .expect("no path given");
+    let img = image::open(img_path)
+        .expect("Cannot find the image at the given path");
 }
